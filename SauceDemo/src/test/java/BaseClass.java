301cc275f60bfa0345fbbe48bd04feda0a62a137
Pavel Khamenia <khameniapavel@gmail.com>
2025-08-04 22:38:52 +0300

    Add

---
+++ SauceDemo/src/test/java/BaseClass.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.HashMap;
+
+public class BaseClass {
+
+  WebDriver driver;
+
+  @BeforeMethod
+  public void setup() {
+    ChromeOptions options = new ChromeOptions();
+    HashMap<String, Object> chromePrefs = new HashMap<>();
+    chromePrefs.put("credentials_enable_service", false);
+    chromePrefs.put("profile.password_manager_enabled", false);
+    options.setExperimentalOption("prefs", chromePrefs);
+    options.addArguments("--disable-popup-blocking");
+    options.addArguments("--disable-infobars");
+    options.addArguments("--start-maximized");//максимальный экран
+    options.addArguments("--incognito");//инкогнито
+    options.addArguments("--disable-notification");//убрать уведомленияя
+    options.addArguments("--headless");//без открытия интерфейса
+    driver = new ChromeDriver();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void teardown() {
+    driver.quit();
+  }
+}
