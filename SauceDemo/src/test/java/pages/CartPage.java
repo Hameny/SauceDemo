bac4885de68170dd47fc61012f8751dd2a928af9
Pavel Khamenia <khameniapavel@gmail.com>
2025-08-05 21:50:23 +0300

    Add Page Obgect

--- SauceDemo/src/test/java/pages/CartPage.java
+++ SauceDemo/src/test/java/pages/CartPage.java
@@ -1,10 +1,21 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CartPage extends BasePage {
 
   public CartPage(WebDriver driver) {
     super(driver);
+  }
+
+  public void open() {
+    driver.get(BASE_URL + "inventory.html");
+  }
+
+  public void addToCard(String product) {
+    driver.findElement(By.xpath("//div[text()='" + product + "']" +
+        "/ancestor::div[@class='inventory_item']"
+        + "//div[@data-test='inventory-item-price']")).click();
   }
 }
