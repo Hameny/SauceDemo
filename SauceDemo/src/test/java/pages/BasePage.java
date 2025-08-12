fe2fe0efef0073004e2da2173f24f7d4cd1caee3
Pavel Khamenia <khameniapavel@gmail.com>
2025-08-12 20:11:28 +0300

    Update

--- SauceDemo/src/test/java/pages/BasePage.java
+++ SauceDemo/src/test/java/pages/BasePage.java
@@ -3,9 +3,12 @@ package pages;
 import java.time.Duration;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
+
   WebDriver driver;
   WebDriverWait wait;
 
@@ -16,8 +19,18 @@ public class BasePage {
     wait = new WebDriverWait(driver, Duration.ofSeconds(20));
   }
 
-  public void clickJs(){
+  public void clickJs(WebElement element) {
     JavascriptExecutor js = (JavascriptExecutor) driver;
-    js.executeScript("argument[0].click;",element);
+    js.executeScript("argument[0].click;", element);
+  }
+
+  public void waitForPageLoaded() {
+    new ExpectedCondition<Boolean>() {
+      public Boolean apply(WebDriver driver) {
+        return ((JavascriptExecutor) driver)
+            .executeScript("return document.readyState")
+            .toString().equals("complete");
+      }
+    };
   }
 }
