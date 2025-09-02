bc3279399c1e33cc8738533fb4adf0232b2fb9ec
Pavel Khamenia <khameniapavel@gmail.com>
2025-09-02 21:45:39 +0300

    Add залогировать SauceDemo, реализовать там цепочку событий.

--- SauceDemo/src/test/java/tests/BaseTest.java
+++ SauceDemo/src/test/java/tests/BaseTest.java
@@ -29,6 +29,8 @@ public class BaseTest {
   ProductsPage productsPage;
   CheckoutPage checkoutPage;
   CartPage cartPage;
+  String user = System.getProperty("user");
+  String password = System.getProperty("password");
 
   @Parameters({"browser"})
   @BeforeMethod(alwaysRun = true, description = "Настройка драйвера")
